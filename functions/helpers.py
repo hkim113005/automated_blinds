eae5668c97e2e54adb431f65c21c9e74a599a207
Hyungjae Kim <hyungjae1130@gmail.com>
2021-11-26 23:11:52 +0400

    Fixed log in helper.py

--- functions/helpers.py
+++ functions/helpers.py
@@ -1,57 +1,111 @@
 from functions.blinds import *
 
-log = open("log.txt", "w+")
+inf = -1000000000
 
-data = log.readline()
+blindMin = inf
+blindMax = inf
+blindCur = 0
 
-blindMin = None
-blindMax = None
-blindCur = None
+def loadLog():
+    global blindMin, blindMax, blindCur, log
+    
+    log = open("log.txt", "r")
 
-mode = 0
-prev = 0
-for i in len(data):
-    if data[i] == " " or data[i] == "\n":
-        if mode == 0:
-            blindMin = int(data[prev:i])
-            prev = i + 1
-            mode += 1
-        elif mode == 1:
-            blindMax = int(data[prev:i])
-            prev = i + 1
-            mode += 1
-        else:
-            blindCur = int(data[prev:i])
-            prev = i + 1
-            mode += 1
+    data = log.read()
+
+    mode = 0
+    prev = 0
+    for i in range(len(data)):
+        if data[i] == " " or data[i] == "\n":
+            if mode == 0:
+                blindMin = int(data[prev:i])
+                prev = i + 1
+                mode += 1
+            elif mode == 1:
+                blindMax = int(data[prev:i])
+                prev = i + 1
+                mode += 1
+            else:
+                blindCur = int(data[prev:i])
+                prev = i + 1
+                mode += 1
+
+    log.close()
+    
+    print("Load: ")
+    print(blindMin)
+    print(blindMax)
+    print(blindCur)
 
 def updateLog():
+    global log
+
+    log = open("log.txt", "w")
+
     newData = str(blindMin) + " " + str(blindMax) + " " + str(blindCur) + "\n"
+    
     log.write(newData)
 
+    log.close()
+    
+    print("Upload: ")
+    print(blindMin)
+    print(blindMax)
+    print(blindCur)
+    
 def blindsUp(v=None):
-    if not v and blindMin and blindMax:
+    global blindMin, blindMax, blindCur
+
+    loadLog()
+    
+    if blindCur == blindMax:
+        print("TOP")
+        return None
+    
+    if (not v or v > blindMax - blindCur) and blindMin != inf and blindMax != inf:
         return blindsUp(blindMax - blindCur)
+    elif not v:
+        return None
 
     up(v)
+
+    blindCur += v
+
+    updateLog()
     
     print("BLINDS UP")
     
 def blindsDown(v=None):
-    if not v and blindMin and blindMax:
-        return blindsUp(blindCur - blindMin)
+    global blindMin, blindMax, blindCur
+
+    loadLog()
+    
+    if blindCur == blindMin:
+        print("BOTTOM")
+        return None
+    
+    if (not v or v > blindCur - blindMin) and blindMin != inf and blindMax != inf:
+        return blindsDown(blindCur - blindMin)
+    elif not v:
+        return None
 
     down(v)
+
+    blindCur -= v
+
+    updateLog()
 
     print("BLINDS DOWN")
 
 def setMin():
     global blindMin, blindMax, blindCur
 
-    if blindMin:
+    loadLog()
+
+    if blindMin != inf:
         return None
 
-    if not blindMax:
+    if blindMax == inf:
         blindMin = 0
         blindCur = 0
     else:
@@ -66,25 +120,22 @@ def setMin():
 def setMax():
     global blindMin, blindMax, blindCur
 
-    if blindMax:
+    loadLog()
+
+    if blindMax != inf:
         return None
 
-    if not blindMin:
-        blindMax = blindCur
-    else:
-        blindMax = blindCur
-        if blindMin < 0:
-            blindMax -= blindMin
-            blindMin = 0
-            blindCur = blindMax
+    blindMax = blindCur
 
     updateLog()
 
 def reset():
     global blindMin, blindMax, blindCur
     
-    blindMin = None
-    blindMax = None
+    loadLog()
+    
+    blindMin = inf
+    blindMax = inf
     blindCur = 0
-
-log.close()
+    
+    updateLog()
